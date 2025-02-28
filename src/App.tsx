6ba66b6ba5aea3938f3a9423baffa1f6d56b0b68
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-02-28 20:36:40 +0800

    initialize rsbuild project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-neutral-800">
+      <h1 className="text-6xl">Rsbuild with React</h1>
+      <p className="text-3xl">Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
